565d921e8cec574d9a8f6d73e8b79359ad6e1161
Manish Rathore <170126019+manishrathore77@users.noreply.github.com>
2026-02-16 17:07:59 +0530

    test: add basic application context test

---
+++ src/test/java/com/thinking/machines/retro/RetroApplicationTests.java
@@ -0,0 +1,15 @@
+package com.thinking.machines.retro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+    
+
