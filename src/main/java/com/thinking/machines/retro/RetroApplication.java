ba57e9848575a0c39b71a69ec7d082027ffe67c2
Manish Rathore <170126019+manishrathore77@users.noreply.github.com>
2026-05-27 12:47:28 +0530

    refactor: clean up RetroApplication entry point

--- src/main/java/com/thinking/machines/retro/RetroApplication.java
+++ src/main/java/com/thinking/machines/retro/RetroApplication.java
@@ -1,3 +1,19 @@
+/*
+public class RetroApplication {
+    public static void main(String[] args) throws Exception {
+        User user = new User();
+        user.setName("Manish Seller");
+        user.setEmail("manishrathoreo273@gmail.com");
+        user.setPhone("8827304100");
+        user.setPassword("manish");
+        user.setUserType("seller");
+
+        UserDAO dao = new UserDAO();
+        dao.addUser(user);
+
+        System.out.println("User inserted successfully!");
+    }
+*/
 package com.thinking.machines.retro;
 
 import org.springframework.boot.SpringApplication;
@@ -8,4 +24,6 @@ public class RetroApplication {
     public static void main(String[] args) {
         SpringApplication.run(RetroApplication.class, args);
     }
+
+
 }
